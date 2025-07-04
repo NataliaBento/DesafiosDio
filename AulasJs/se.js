cb13c87e65fd0e7e92d2fab7286ddbc58b6fda2b
Natalia Bento <nataliabento97@gmail.com>
2025-07-04 14:48:31 -0300

    feat: adiciona mais um desafio

---
+++ AulasJs/se.js
@@ -0,0 +1,9 @@
+
+
+const numero = prompt('Digite um numero e veja se é par')
+
+if (numero % 2 === 0){
+    console.log('Seu numero é par')
+} else {
+    console.log('Seu numero é ímpar')
+}
---
+++ AulasJs/segundo_desafio.js
@@ -0,0 +1,28 @@
+//Faça um programa para calcular o valor de uma viagem.
+
+//Você terá 5 variáveis. Sendo elas:
+// 1- Preço do etanol;
+// 2- Preço do gasolina;
+// 3- O tipo de combustível que está no seu carro;
+// 4- Gasto médio de combustível do carro por KM;
+// 5 - Distância em KM da viagem;
+
+const precoEtanol = 5.79;
+
+const precoGasolina = 6.66;
+
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const tipoCombustível = 'Etanol';
+
+const litrosCosumidos = distanciaEmKm / kmPorLitros;
+
+if (tipoCombustível === "Etanol"){
+    const valorGasto = litrosCosumidos * precoEtanol
+    console.log('R$' +valorGasto.toFixed(2));
+}else{
+    const valorGasto = litrosCosumidos * precoGasolina
+    console.log('R$' +valorGasto.toFixed(2));
+}
+
